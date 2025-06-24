4cb1fdf7e33001a5d3ceda6a43c08107a4e034be
justrunme <52860508+justrunme@users.noreply.github.com>
2025-06-24 21:49:44 +0200

    Update servicemonitor.tf

--- servicemonitor.tf
+++ servicemonitor.tf
@@ -1,20 +1,35 @@
-# Ждём появления CRD (один раз, не дублировать в других файлах)
+# Prometheus stack ставит CRD
+resource "helm_release" "kube_prometheus_stack" {
+  name       = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "58.6.0"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+
+  values = [
+    file("${path.module}/monitoring-values.yaml")
+  ]
+
+  depends_on = [kubernetes_namespace.monitoring]
+}
+
+# Wait for ServiceMonitor CRD
 resource "null_resource" "wait_for_servicemonitor_crd" {
   depends_on = [helm_release.kube_prometheus_stack]
-
   provisioner "local-exec" {
     command = <<EOT
-for i in {1..30}; do
-  kubectl get crd servicemonitors.monitoring.coreos.com && exit 0
-  echo "Waiting for ServiceMonitor CRD..."
-  sleep 5
-done
-echo "CRD ServiceMonitor not found after timeout!" >&2
-exit 1
-EOT
+      for i in {1..30}; do
+        kubectl get crd servicemonitors.monitoring.coreos.com && exit 0
+        echo "Waiting for ServiceMonitor CRD..."
+        sleep 5
+      done
+      echo "CRD ServiceMonitor not found after timeout!" >&2
+      exit 1
+    EOT
   }
 }
 
+# ServiceMonitor (создастся только после появления CRD)
 resource "kubernetes_manifest" "test_app_servicemonitor" {
   manifest = {
     apiVersion = "monitoring.coreos.com/v1"
@@ -34,7 +49,7 @@ resource "kubernetes_manifest" "test_app_servicemonitor" {
       }
       endpoints = [
         {
-          port     = "http"     # Имя порта из kubernetes_service.test_app
+          port     = "http"     # Имя порта в Service!
           path     = "/"
           interval = "15s"
         }
